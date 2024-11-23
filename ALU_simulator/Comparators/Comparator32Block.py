4fb8b5d719be64a2d1d8c3f47beefcedf252e822
Nemo <truongng201@gmail.com>
2024-11-23 11:04:51 +0700

    feat: minor update

--- ALU_simulator/Comparators/Comparator32Block.py
+++ ALU_simulator/Comparators/Comparator32Block.py
@@ -20,24 +20,22 @@ class Comparator32Block:
         
         
     def __select_operation(self):
-        operation3 = int(self.__operation[0])
-        operation2 = int(self.__operation[1])
-        operation1 = int(self.__operation[2])
-        operation0 = int(self.__operation[3])
+        operation3 = int(self.__operation[0]) == 1
+        operation2 = int(self.__operation[1]) == 1
+        operation1 = int(self.__operation[2]) == 1
+        operation0 = int(self.__operation[3]) == 1
         
         # operation3 and not operation2 and not operation1 and not operation0
-        first_min_term = operation3 & (~ operation2) & (~ operation1) & (~ operation0)
+        first_min_term = operation3 and (not operation2) and (not operation1) and (not operation0)
         # operation3 and not operation2 and not operation1 and operation0
-        second_min_term = operation3 & (~ operation2) & (~ operation1) & operation0
+        second_min_term = operation3 and (not operation2) and (not operation1) and operation0
         # operation3 and operation2 and operation1 and not operation0
-        third_min_term = operation3 & operation2 & operation1 & (~ operation0)
+        third_min_term = operation3 and operation2 and operation1 and (not operation0)
         # operation3 and operation2 and operation1 and operation0
-        fourth_min_term = operation3 & operation2 & operation1 & operation0
+        fourth_min_term = operation3 and operation2 and operation1 and operation0
         
-        self.__enable_mux = str(first_min_term | second_min_term | third_min_term | fourth_min_term)
-        second_or = str(second_min_term | fourth_min_term)
-        third_or = str(third_min_term | fourth_min_term)
-        self.__select_bits = third_or + second_or
+        self.__enable_mux = str(int(first_min_term or second_min_term or third_min_term or fourth_min_term))
+        self.__select_bits = str(int(second_min_term | fourth_min_term)) + str(int(third_min_term | fourth_min_term))
 
 
     def __validate_input(self):
